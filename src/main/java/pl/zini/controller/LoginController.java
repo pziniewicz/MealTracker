f967dc4c51bbae02afe38064fc9d8d1e459fde41
Pawel Zini <83308656+pziniewicz@users.noreply.github.com>
2021-09-10 09:37:45 +0200

    merge

---
+++ src/main/java/pl/zini/controller/LoginController.java
@@ -0,0 +1,10 @@
+package pl.zini.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login111")
+public class LoginController {
+
+}
